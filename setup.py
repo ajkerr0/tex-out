b156a48fd76d1c937584fa3086c12c053ee96ca5
Alex Kerr <ajkerr0@gmail.com>
2017-04-05 01:52:22 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup
+
+setup(name="tex-out",
+      version="0.1.0",
+      description="A module for outputting to text files.",
+      author="Alex Kerr",
+      author_email="ajkerr0@gmail.com",
+      url="https://github.com/ajkerr0/tex-out",
+      packages=find_packages(),
+      install_requires=[],
+      )
